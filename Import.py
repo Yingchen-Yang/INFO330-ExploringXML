30379ac9d06b008605d14acbb77ddec793b3626b
Yingchen-Yang <129204557+Yingchen-Yang@users.noreply.github.com>
2023-05-10 16:29:45 -0700

    extra credit

--- Import.py
+++ Import.py
@@ -19,19 +19,23 @@ for i, arg in enumerate(sys.argv):
 
     for pokemon_elem in root.findall("pokemon"):
         name = pokemon_elem.find("name").text
-        pokedex_number = pokemon_elem.get("pokedex_number")
-        classification = pokemon_elem.get("classification")
+        pokedex_number = pokemon_elem.get("pokedexNumber")
         generation = pokemon_elem.get("generation")
         hp = pokemon_elem.find("hp").text
-        type1name = pokemon_elem.find("type1name").text
-        type2name = pokemon_elem.find("type2name").text
         attack = pokemon_elem.find("attack").text
         defense = pokemon_elem.find("defense").text
         speed = pokemon_elem.find("speed").text
         sp_attack = pokemon_elem.find("sp_attack").text
         sp_defense = pokemon_elem.find("sp_defense").text
-        height_m = pokemon_elem.find("height_m").text
-        weight_kg = pokemon_elem.find("weight_kg").text
+        height_m = pokemon_elem.find("height").text
+        weight_kg = pokemon_elem.find("weight").text
+
+    for pokemon_elem in root.findall("pokemon_types_view"):
+        type1name = pokemon_elem.find("type").text
+        type2name = pokemon_elem.find("type2").text
+
+    for pokemon_elem in root.findall("classification"):
+        classification = pokemon_elem.get("classification")
 
         try:
             cursor.execute("INSERT INTO pokemon (name, pokedex_number, generation, hp, attack, defense, speed, sp_attack, sp_defense, height_m, weight_kg) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
